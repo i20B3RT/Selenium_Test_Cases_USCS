b6cfe6ff091bf2a124737a527ab03d95d1750bba
i20B3RT <i20B3RT@live.com>
2017-10-28 22:53:49 -0400

    Java 1.8 for the languagelevel did not work with mac, had to change it to 1.7 jdk

--- src/chromeTestCase.java
+++ src/chromeTestCase.java
@@ -2,7 +2,9 @@
  * Created by fwrmoral on 10/28/2017.
  */
 //package com.demo.testcases;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
@@ -10,19 +12,44 @@ public class chromeTestCase {
 
     public static void main(String[] args) {
         //TODO auto generate method stub
+        String myURL = ("http://10.1.100.197:9109");
+        String myLOGIN =("/ewm/login.html");
+        String myHOME =("/ewm/home.htm");
 
         String os = System.getProperty("os.name").toLowerCase();
 
         WebDriver driver = new ChromeDriver();
 
         if (os.contains("mac")) {
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "i20B3RT/Google Drive/Selenium/Chrome_Driver/");
         } else {
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "fwrmoral/Google Drive/Selenium/Chrome_Driver/chromedriver.exe");
         }
 
 
-        driver.get("http://www.google.com");
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        // Open eWM login page
+        driver.get(myURL+myLOGIN);
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        // Maximize browser
+        driver.manage().window().maximize();
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        //input username
+        WebElement myElement = driver.findElement(By.name("j_username"));
+        myElement.sendKeys("fwrmoral");
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        //input password
+        WebElement myPass = driver.findElement(By.name("j_password"));
+        myPass.sendKeys("Password123");
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        //click on the submit button to login
+        driver.findElement(By.name("btn_login")).click();
 
     }
 }
